55508ea1d1e38f23009b76978fd9b971ae0dc8fb
Julia <cudina.iuk@gmail.com>
2024-04-13 13:52:15 +0300

    stderr, stdout, stdin

---
+++ project_03_process/ex_01_start.js
@@ -0,0 +1,29 @@
+//import process from 'node:process';
+import { argv, stdout, stderr, exit } from 'node:process';
+
+console.log('argv 1', argv[2]);
+console.log('argv 2', argv[3]);
+console.log('argv 3', argv[4]);
+
+stdout.write("Success! Example stdout\n");
+stderr.write("error! some error occurred\n");
+
+//exit(0);
+//exit(1);
+//exit(2);
+
+exit(42);
+
+// let data: string  = '';
+//
+// process.stdin.on('readable', () =>{
+//    const chunk = process.stdin.read();
+//    if(chunk !== null) {
+//        data += chunk;
+//    }
+// });
+//
+// process.stdin.on('end', () => {
+//     console.log('Data', data);
+//     process.exit(0);
+// });
